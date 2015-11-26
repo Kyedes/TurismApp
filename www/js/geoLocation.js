0381d81517bacb9069f5fdc85e0f236b85716d25
Kyedes <esky13ab@student.cbs.dk>
2015-11-26 20:35:30 +0100

    Proof of concept done.

--- www/js/geoLocation.js
+++ www/js/geoLocation.js
@@ -5,102 +5,109 @@
 angular.module('starter.geoLocation', [])
 
 
-    .controller('MapGeoCtrl', function ($scope, $ionicLoading, $ionicPopup, GeoList) {
+  .controller('MapGeoCtrl', function ($scope, $ionicLoading, $ionicPopup, GeoList, Camera) {
 
-        $scope.positions = GeoList.getAll();
-        $scope.mon = {dis:100, monument:null};
-        $scope.$on('mapInitialized', function (event, map) {
-            $scope.map = map;
+    $scope.positions = GeoList.getAll();
+    $scope.mon = {dis:100, monument:null};
+
+    $scope.$on('mapInitialized', function (event, map) {
+      $scope.map = map;
+    });
+
+    $scope.centerOnMe = function () {
+
+      //$scope.getPhoto = function() {
+        Camera.getPicture().then(function(imageURI) {
+          $scope.photo = imageURI;
+        }, function(err) {
+          console.err(err);
+        })
+      //}
+
+      //camera.getPicture().then(function(imageURI) {
+       // $scope.photo = imageURI;
+
+        // imageURI is the URL of the image that we can use for
+        // an <img> element or backgroundImage.
+
+      //}, function(err) {
+        // Ruh-roh, something bad happened
+     //   console.err(err);
+     // }, cameraOptions);
+
+
+      //If you want
+      //$scope.positions = [];
+
+
+      /* Splash */
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
+
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      function success(position) {
+
+        var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        // $scope.positions.push({lat: position.coords.latitude, lng: position.coords.longitude}); //Adds location to array?
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        $scope.map.setCenter(pos);
+        $scope.distances = [];
+        angular.forEach($scope.positions, function (mark) {
+
+          var dis = calculateDistance(position.coords, mark);
+
+          //console.log((dis / 1000) + " km")
+          console.log(dis, mark.monument);
+          if (dis <= 10) if (dis < $scope.mon.dis)$scope.mon = {dis:dis, monument: mark};
+
         });
-        $scope.centerOnMe = function () {
-
-            camera.getPicture().then(function(imageURI) {
-            $scope.photo = imageURI;
-
-            // imageURI is the URL of the image that we can use for
-            // an <img> element or backgroundImage.
-
-          }, function(err) {
-            // Ruh-roh, something bad happened
-            console.err(err);
-          }, cameraOptions);
+        if ($scope.mon.monument == null)$scope.mon = {dis:100, monument: {monument: "Error 404", description:"No monument found within 10 meters"}};
+        console.log($scope.mon);
 
 
-            //If you want
-            //$scope.positions = [];
+        $ionicLoading.hide();
+
+      }
+
+      function error(error) {
+        console.log(error);
+        $ionicLoading.hide();
+        $ionicPopup.alert({
+          title: "Error code: " + error.code,
+          template: error.message
+        });
+      }
+
+      // This is our get position function!
+      navigator.geolocation.getCurrentPosition(success, error, options);
 
 
-            /* Splash */
-            $ionicLoading.show({
-                template: 'Loading...'
-            });
+      /* function to calc radius */
+      var rad = function (x) {
+        return x * Math.PI / 180;
+      };
 
-            var options = {
-                enableHighAccuracy: true,
-                timeout: 5000,
-                maximumAge: 0
-            };
-
-            function success(position) {
-
-              var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-              // $scope.positions.push({lat: position.coords.latitude, lng: position.coords.longitude}); //Adds location to array?
-                console.log(position.coords.latitude);
-                console.log(position.coords.longitude);
-                $scope.map.setCenter(pos);
-                $scope.distances = [];
-                angular.forEach($scope.positions, function (mark) {
-
-                    var dis = calculateDistance(position.coords, mark);
-
-                    //console.log((dis / 1000) + " km")
-                  console.log(dis, mark.monument);
-                  if (dis <= 10) if (dis < $scope.mon.dis)$scope.mon = {dis:dis, monument: mark};
-
-
-
-
-
-                });
-              console.log($scope.mon);
-
-
-                $ionicLoading.hide();
-
-            }
-
-            function error(error) {
-                console.log(error);
-                $ionicLoading.hide();
-                $ionicPopup.alert({
-                    title: "Error code: " + error.code,
-                    template: error.message
-                });
-            }
-
-            // This is our get position function!
-            navigator.geolocation.getCurrentPosition(success, error, options);
-
-
-            /* function to calc radius */
-            var rad = function (x) {
-                return x * Math.PI / 180;
-            };
-
-            /* CALCULATE DISTANCE */
-            function calculateDistance(p1, p2) {
-                var R = 6378137; // Earth’s mean radius in meter
-                var dLat = rad(p2.lat - p1.latitude);
-                var dLong = rad(p2.lng - p1.longitude);
-                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                    Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.lat)) *
-                    Math.sin(dLong / 2) * Math.sin(dLong / 2);
-                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-                var d = R * c;
-                return d; // returns the distance in meter
-            }
-        }
-    })
+      /* CALCULATE DISTANCE */
+      function calculateDistance(p1, p2) {
+        var R = 6378137; // Earth’s mean radius in meter
+        var dLat = rad(p2.lat - p1.latitude);
+        var dLong = rad(p2.lng - p1.longitude);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.lat)) *
+          Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        return d; // returns the distance in meter
+      }
+    }
+  })
 
 
 
@@ -121,7 +128,6 @@ angular.module('starter.geoLocation', [])
       }
     }
   }])
-
 
   .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
